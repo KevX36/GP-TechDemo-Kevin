7fcc63d66c696010c8e840209392b9eb5ca47b8d
KevX36 <kevincard2007@gmail.com>
2026-03-03 21:18:19 -0400

    fixed phantom colider, nolong uses charater controller, need to fix jump function

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,24 +3,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController Controller;
-
-    //movement
+    //player parts
     private Rigidbody rb;
-    private Vector3 MoveDirection;
+    private Collider col;
+    //movement
+
+    [SerializeField] private Vector3 MoveDirection;
     public int speed = 5;
     //jump
     Vector3 PlayerFall = new Vector3 (0,0,0);
     public int Gravity = 5;
     public int Jumphighet = 7;
-    private bool Jumping = false;
-
+    [SerializeField] private bool Jumping = false;
+    [SerializeField]private bool isGrounded = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        col = this.GetComponent<Collider>();
         rb = this.GetComponent<Rigidbody>();
-        Controller = this.GetComponent<CharacterController>();
+        if(Gravity > 0)
+        {
+            Gravity *= -1;
+        }
         MoveDirection = rb.position;
     }
     public void OnJump(InputAction.CallbackContext Context)
@@ -29,8 +34,9 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Jumped");
             Jumping = true;
-            PlayerFall.y = Jumphighet * Gravity;
+            
         }
+        PlayerFall.y = Jumphighet * -Gravity;
         if (Context.canceled)
         {
             Jumping = false;
@@ -42,14 +48,22 @@ public class PlayerController : MonoBehaviour
         Vector2 direction2d = Context.ReadValue<Vector2>();
         MoveDirection = new Vector3(direction2d.x, PlayerFall.y, direction2d.y);
     }
-    // Update is called once per frame
+    public bool cheakIfGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up,col.bounds.extents.y + 0.1f);
+
+        
+    }
     void Update()
     {
-        if (Controller.isGrounded && !Jumping)
+        isGrounded = cheakIFGrounded();
+        
+        if (isGrounded && !Jumping)
         {
+            Debug.Log("isGround");
             PlayerFall.y = 0;
         }
-        PlayerFall.y -= Gravity * Time.deltaTime;
+        PlayerFall.y += Gravity * Time.deltaTime;
         rb.MovePosition(rb.position + MoveDirection * speed * Time.deltaTime);
     }
 }
